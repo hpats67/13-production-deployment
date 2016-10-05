3d98e2123f0071fe89ac47851032a6c8e04c2211
Steven Jackson <hpats67@gmail.com>
2016-10-05 13:37:27 -0700

    Finished first TODO, added AJAX call

--- scripts/models/repo.js
+++ scripts/models/repo.js
@@ -5,11 +5,27 @@
 
   reposObj.requestRepos = function(callback) {
     // TODO: refactor this request into an $.ajax call
-    $.get('/github/users/codefellows-portland-301d6/repos' +
-          '?per_page=10&sort=updated')
-          .done(function(data) {
-            reposObj.allRepos = data;
-          }).done(callback);
+    $.ajax({
+      url: '/github/users/codefellows-portland-301d6/repos' +
+            '?per_page=10&sort=updated',
+      method: 'GET',
+      success: successHandler,
+      error: errorHandler
+    });
+
+    function successHandler(data) {
+      reposObj.allRepos = data;
+      callback();
+    };
+
+    function errorHandler(error) {
+      console.log('ERROR', error);
+    };
+  //   $.get('/github/users/codefellows-portland-301d6/repos' +
+  //         '?per_page=10&sort=updated')
+  //         .done(function(data) {
+  //           reposObj.allRepos = data;
+  //         }).done(callback);
   };
 
   reposObj.withTheAttribute = function(attr) {
